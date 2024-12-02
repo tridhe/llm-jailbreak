1cbe24004351cd572f9988f5f0367f0e1d4576a8
Tenzin Tridhe Naga <tridhe@Tenzins-MacBook-Pro-2.local>
2024-12-02 15:55:32 +0100

    Updates

--- src/components/TypingIndicator.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import MESSAGES from "./messages";
-
-const TypingIndicator = () => {
-  return <div className="typing-indicator">{MESSAGES.santaIsTyping}</div>;
-};
-
-export default TypingIndicator;
